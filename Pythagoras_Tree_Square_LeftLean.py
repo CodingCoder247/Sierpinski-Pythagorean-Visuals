382837088a9116320a719240028f4b5040db13f6
CodingCoder247 <137262231+CodingCoder247@users.noreply.github.com>
2024-02-21 09:57:38 +0800

    Update Pythagoras_Tree_Square_LeftLean.py

--- Pythagoras_Tree_Square_LeftLean.py
+++ Pythagoras_Tree_Square_LeftLean.py
@@ -1,11 +1,3 @@
-'''
-Name: Syarifah Radhiyah D/O Mohd Rafi
-Admin Number: 231816M
-Tutorial Group: IT1312-02
-Question: 2a(ii)
-Description: Pythagoras Tree Left Lean
-'''
-
 import turtle  # Import the turtle graphics library for drawing
 import math  # Import the math library for mathematical operations
 
